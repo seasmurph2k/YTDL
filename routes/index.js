6d857ae21bcfa37703cb50abf659c91739e7563a
Connor Murphy <seasmurph@gmail.com>
2018-10-02 22:55:50 +0100

    Validation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const validator = require("validator");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", {
+    errors: ""
+  });
 });
+
+/* POST download page */
+router.post(
+  "/convert",
+  //Validation
+  (req, res, next) => {
+    let regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)youtube.com\/watch\?v=/g;
+
+    if (!validator.isURL(req.body.youtubeURL)) {
+      res.render("index", {
+        errors: { error: "Must be a valid URL" }
+      });
+    } else if (req.body.youtubeURL.match(regex) === null) {
+      //Will be null if it doesn't match or an array if it does
+      res.render("index", {
+        errors: { error: "Must be a valid youtube URL" }
+      });
+    } else {
+      next();
+    }
+  },
+  (req, res, next) => {}
+);
 
 module.exports = router;
